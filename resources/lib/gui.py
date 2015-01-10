38c9f8c78eecd696088934e9faf465ff0c985a58
kinnairdclan <kinnard.jason@gmail.com>
2015-01-09 21:22:09 -0700

    Error checking, percentage progress bar now works

--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -17,18 +17,21 @@ __addonname__ = __addon__.getAddonInfo('name')
 ACTION_PREVIOUS_MENU = 10
 KEY_BUTTON_BACK = 275
 
+#Global job
+MAKEMKVCON = None
+
 class mkvripper_gui(xbmcgui.WindowDialog):
 
     def __init__(self):
 
+        global MAKEMKVCON
         dialog = xbmcgui.Dialog()
+        makemkvcon_debug_on = plugin.get('makemkvcon_debug', False)
 
         if not makemkvcon.installed():
             plugin.log(plugin.lang(50007)) #makemkvcon binary not found
 
-            dialog.ok(plugin.lang(50007), 
-                      plugin.lang(50008),
-                      plugin.lang(50009))
+            dialog.ok(plugin.lang(50007), plugin.lang(50008), plugin.lang(50009))
             self.close()
             return
 
@@ -66,24 +69,71 @@ class mkvripper_gui(xbmcgui.WindowDialog):
                     return
 
                 p_dialog = xbmcgui.DialogProgress() #here you might want to roll a special progress window that shelves the job onto service and wakes up service 
-                p_dialog.create(__addonname__, plugin.lang(50005) % job['dev']) #Scanning media in <disc>
-                job = makemkvcon.start(job)
-                pipe = job['output']
-                msg = plugin.lang(50006) #Ripping files from media in <disc>
-
+                MAKEMKVCON = makemkvcon.start(job)
+                p_dialog.create(__addonname__, plugin.lang(50005) % MAKEMKVCON['dev']) #Scanning media in <disc>
+                pipe = MAKEMKVCON['output']
+                label = plugin.lang(50006) % (MAKEMKVCON['dev'], MAKEMKVCON['tmp_dir']) #Ripping files from media in <disc> to temporary directory <tmp_dir>
+                percent = 0
+                
                 while True:
                     line = pipe.stdout.readline()
                     if not line:
                         break
+
                     if line.startswith('MSG:'):
-                        p_dialog.update(0, msg % job['dev'], line.split('"')[1])
-                        sys.stdout.flush()
+                        msg = line.split('"')[1]
+                        if line.startswith('MSG:5038'):                                  #seems like a rather lame way to catch an error, but this is all makemkvcon's giving me
+                            makemkvcon.kill(MAKEMKVCON, '-STOP')                                #pause makemkvcon
+                            if dialog.yesno(__addonname__, msg):                         #display makemkvcon prompt 
+                                line = ''                                                #get rid of message so progress dialog does not display it
+                                makemkvcon.kill(MAKEMKVCON, '-CONT')                            #continue makemkvcon if agree
+                            else:
+                                sys.stdout.flush()
+                                makemkvcon.kill(MAKEMKVCON)
+                                MAKEMKVCON = None
+                                self.close()
+                                return
+                        if line.startswith('MSG:2018'):
+                            makemkvcon.kill(MAKEMKVCON, '-STOP')
+                            if dialog.ok(plugin.lang(50015), msg): 
+                                sys.stdout.flush()
+                                makemkvcon.kill(MAKEMKVCON)
+                                MAKEMKVCON = None
+                                self.close()
+                                return
+                        if makemkvcon_debug_on:
+                            if msg.startswith('Error:'):
+                                makemkvcon.kill(MAKEMKVCON, '-STOP')
+                                if dialog.ok(plugin.lang(50015), msg): 
+                                    sys.stdout.flush()
+                                    makemkvcon.kill(MAKEMKVCON)
+                                    MAKEMKVCON = None
+                                    self.close()
+                                    return
+
+                    if line.startswith('PRGV:'):        
+                        line = line.split(',')
+                        n = float(line[1])
+                        d = float(line[2])
+                        percent = int(n / d * 100)
+
+                    p_dialog.update(percent, label, msg)
+                    sys.stdout.flush()
+
                     if p_dialog.iscanceled():
-                        makemkvcon.kill(job)
-                        plugin.notify(plugin.lang(50014) % job['dev'])
+                        #makemkvcon.kill(MAKEMKVCON)
+                        self.close()
                         return
-                makemkvcon.save(job)
-                plugin.notify(plugin.lang(50013) % job['dev'])
+
+                p_dialog.update(100, plugin.lang(50016) % MAKEMKVCON['dest_writepath']) #Moving titles from temporary directory to <savedir>
+                try:
+                    makemkvcon.save(MAKEMKVCON)
+                except Exception, e:
+                    if dialog.ok(plugin.lang(50015), str(e)):
+                        self.close()
+                        return
+                plugin.notify(plugin.lang(50013) % (MAKEMKVCON['dev'], MAKEMKVCON['dest_writepath']), timeout=10000) #Rip finished on <disc>. New files in <savedir>
+                MAKEMKVCON = None
             else:
                 plugin.log(plugin.lang(50010)) #User did not select a disc rip operation.
                 self.close()
@@ -92,3 +142,21 @@ class mkvripper_gui(xbmcgui.WindowDialog):
         if action == ACTION_PREVIOUS_MENU or KEY_BUTTON_BACK:
             self.close()
             pass
+
+    def close(self):
+        global MAKEMKVCON
+        if MAKEMKVCON is not None:
+            makemkvcon.kill(MAKEMKVCON, '-STOP')
+            if xbmcgui.Dialog().yesno(__addonname__, 'Cancel disc rip on %s or run in background?' % MAKEMKVCON['dev'], nolabel='Cancel', yeslabel='Run in background'):
+                makemkvcon.kill(MAKEMKVCON, '-CONT')
+                MAKEMKVCON = None
+                super(mkvripper_gui, self).close()
+            else:
+                makemkvcon.kill(MAKEMKVCON)
+                plugin.notify(plugin.lang(50014) % MAKEMKVCON['dev'])
+                super(mkvripper_gui, self).close()
+        else:
+            super(mkvripper_gui, self).close()
+
+
+
